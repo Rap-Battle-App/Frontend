7552ab01bb72b1cda9884350a1fc16971ce75b5a
Mark Kastner <kastner.mark@gmail.com>
2015-12-09 21:15:20 +0100

    Neu

--- Rapbattle16Bars/app/src/main/java/com/batllerap/hsosna/rapbattle16bars/MainActivity.java
+++ Rapbattle16Bars/app/src/main/java/com/batllerap/hsosna/rapbattle16bars/MainActivity.java
@@ -102,7 +102,8 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_logout:
                 Intent d = new Intent(this, Login.class);
                  if(aktUser != null){
-                     authController.logout(aktUser.getUserName());
+                     //TODO WIEDER EINKOMMENTIEREN
+                     //authController.logout(aktUser.getUserName());
                  }
 
                 startActivity(d);

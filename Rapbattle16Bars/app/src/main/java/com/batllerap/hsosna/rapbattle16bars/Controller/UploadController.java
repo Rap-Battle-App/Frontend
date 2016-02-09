2c22c1724032948f74846b81c8f5379b1570df45
Mark Kastner <kastner.mark@gmail.com>
2016-02-09 21:03:27 +0100

    Code optimierung

---
+++ Rapbattle16Bars/app/src/main/java/com/batllerap/hsosna/rapbattle16bars/Controller/UploadController.java
@@ -0,0 +1,133 @@
+package com.batllerap.hsosna.rapbattle16bars.Controller;
+
+import android.os.AsyncTask;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+/**
+ * Created by Mark on 14.01.2016.
+ */
+public class UploadController extends AsyncTask<File, Void, Void> {
+
+    String lineEnd = "\r\n";
+    String twoHyphens = "--";
+    String boundary = "AaB03x87yxdkjnxvi7";
+
+    protected Void doInBackground(File... files){
+        File file = files[0];
+        URL url = null;
+        try {
+            url = new URL("http://46.101.216.34/profile/picture");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        HttpURLConnection conn = null;
+        DataOutputStream dos = null;
+        DataInputStream dis = null;
+        FileInputStream fileInputStream = null;
+
+        byte[] buffer;
+        int maxBufferSize = 20 * 1024;
+        try {
+            // ------------------ CLIENT REQUEST
+            fileInputStream = new FileInputStream(file);
+
+            // open a URL connection to the Servlet
+            // Open a HTTP connection to the URL
+            conn = (HttpURLConnection) url.openConnection();
+            // Allow Inputs
+            conn.setDoInput(true);
+            // Allow Outputs
+            conn.setDoOutput(true);
+            // Don't use a cached copy.
+            conn.setUseCaches(false);
+            // Use a post method.
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type",
+                    "multipart/form-data;boundary=" + boundary);
+
+            dos = new DataOutputStream(conn.getOutputStream());
+
+            dos.writeBytes(twoHyphens + boundary + lineEnd);
+            dos.writeBytes("Content-Disposition: form-data; name=\""
+                    + "picture" + "\"; filename=\"" + "profilePicture"
+                    + ".jpg" + "\"" + lineEnd);
+            dos.writeBytes("Content-Type:image/jpg" + lineEnd);
+            dos.writeBytes(lineEnd);
+
+            // create a buffer of maximum size
+            buffer = new byte[Math.min((int) file.length(), maxBufferSize)];
+            int length;
+            // read file and write it into form...
+            while ((length = fileInputStream.read(buffer)) != -1) {
+                dos.write(buffer, 0, length);
+            }
+
+            // send multipart form data necessary after file data...
+            dos.writeBytes(lineEnd);
+            dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+            dos.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null)
+                try {
+                    fileInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            if (dos != null)
+                try {
+                    dos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+
+        // ------------------ read the SERVER RESPONSE
+        try {
+            dis = new DataInputStream(conn.getInputStream());
+            StringBuilder response = new StringBuilder();
+
+            String line;
+            while ((line = dis.readLine()) != null) {
+                response.append(line).append('\n');
+            }
+
+            System.out.println("Upload file responce:"
+                    + response.toString());
+            System.out.println(response.toString());
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (dis != null)
+                try {
+                    dis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+        System.out.println(" --- BilderUpload fertig ---");
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void result) {
+        // TODO Auto-generated method stub
+        super.onPostExecute(result);
+    }
+}
